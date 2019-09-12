9a90112daafc003413445330eb55e5a6c304fc3c
anibaldeboni <anibal.neto@creditas.com.br>
2019-09-12 14:24:55 -0300

    cleanup

--- src/server.js
+++ src/server.js
@@ -1,31 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { name, version } = require('../package.json');
-
-// const morgan = require('morgan');
-// const clientSession = require('client-sessions');
 // const helmet = require('helmet');
-
-// const {SESSION_SECRET} = require('./config');
-
 const app = express();
 const api = require('./api');
 
+app.use(bodyParser.json());
+app.use(api);
+
 app.get('/', (req, res) => res.sendStatus(200));
 app.get('/health', (req, res) => res.sendStatus(200));
-
-// app.use(morgan('short'));
-app.use(bodyParser.json());
-// app.use(
-//   clientSession({
-//     cookieName: 'session',
-//     secret: SESSION_SECRET,
-//     duration: 24 * 60 * 60 * 1000
-//   })
-// );
-// app.use(helmet());
-
-app.use(api);
 
 let server;
 module.exports = {
